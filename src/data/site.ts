d595d163c512402d2bf42185204197f8b43d8f95
marceldavidbaroi <david@itconquest.com>
2026-09-13 00:32:17 +0600

    cv updated

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,7 +2,7 @@ export const site = {
 	name: 'Marcel David Baroi',
 	nameMark: 'MDB',
 	email: 'marceldavidbaroi@gmail.com',
-	linkedin: 'https://linkedin.com/in/marceldavidbaroi',
+	linkedin: 'https://www.linkedin.com/in/marcel-david-baroi/',
 	github: 'https://github.com/marceldavidbaroi',
 	resumePdf: '/resume.pdf',
 	resumeTxt: '/resume.txt'
